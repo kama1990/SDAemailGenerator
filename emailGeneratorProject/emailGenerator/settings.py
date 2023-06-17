2cf41abb970cdc5d275c2aecc16f069b9d64cd6a
kamila <czajkowska.kamila@wp.pl>
2023-06-17 18:52:32 +0200

    covered secret_key

--- emailGeneratorProject/emailGenerator/settings.py
+++ emailGeneratorProject/emailGenerator/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q()vv-jxv%^4r1af-!%1_ak5l5s7!q+*knbyc7ksqxr%i#n(@)'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
